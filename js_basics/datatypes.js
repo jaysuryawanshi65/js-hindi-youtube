5974ee5113205e5749d2ac9dc5426b407332fae0
Jay Suryavanshi <98643211+jaysuryawanshi65@users.noreply.github.com>
2024-10-13 12:29:01 +0000

    changes in the files

---
+++ js_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer 
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "jay"
+let age = 24
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
---
+++ js_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 15007
+let accountEmail="suryavanshijay65@gmail.com"
+var accountPassword="12345"
+accountCity = "jaipur"
+
+//accountId = 2 // not allowed
+
+accountEmail = "pass@gmail.com"
+accountPassword ="123450"
+accountCity = "Miraj"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
