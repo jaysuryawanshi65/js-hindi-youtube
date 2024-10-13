90a3296bd8fd7b966d1c4a73fcfa7d0440d4a239
Jay Suryavanshi <98643211+jaysuryawanshi65@users.noreply.github.com>
2024-10-13 12:57:07 +0000

    conversion of datatypes

---
+++ js_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score = null
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn  = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "jay" => true
+
+let someNumber = 15
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
