e2aa4d62abfc6591f7b3c0680e62af7c2d4ded7e
Jay Suryavanshi <98643211+jaysuryawanshi65@users.noreply.github.com>
2024-10-13 14:27:28 +0000

    datatype-summary

---
+++ js_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive 
+
+// 7 types : String, Number , Boolean , Null, Undefined, Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const bigNumber = 20472384742877432n
+
+
+// Reference Type (non-primitive)
+
+// Array, Objects , Functions
+
+
+const heros1 = ["superman","spiderman","batman"] =
+{
+    name:"jay",
+    age: 24,
+}
+
+console.log(heros1);
+
+
+const myFunction = function(){
+    //console.log("helo world")
+}
+
+//console.log(typeof bigNumber)
