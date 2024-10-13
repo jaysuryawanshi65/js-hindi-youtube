8b95d9b0a4554b8c25ee8c73380b8bcceeef278d
Jay Suryavanshi <98643211+jaysuryawanshi65@users.noreply.github.com>
2024-10-13 13:46:55 +0000

    changes

--- js_basics/conversionOperation.js
+++ js_basics/conversionOperation.js
@@ -21,5 +21,22 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 15
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
+
+
+// ********************operations********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "jay"
+let str2 = " suryavanshi"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
